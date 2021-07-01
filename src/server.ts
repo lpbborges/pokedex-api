86cff4978d40d43e4fa7c8d8e7fb9586f189051b
LP <luizpaulobborges@gmail.com>
2021-06-30 22:59:40 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 
+import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
