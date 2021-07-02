b7d79fe4fe63f7ac279bcbced91f00ba99effcbf
LP <luizpaulobborges@gmail.com>
2021-07-02 11:10:35 -0300

    added yup validation

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,12 +1,22 @@
 import { compare } from "bcrypt";
 import { Request, Response } from "express";
 import { sign } from "jsonwebtoken";
+import * as Yup from "yup";
 
 import { jwtConfig } from "../../constants";
 import { UserModel } from "../models/User";
 
 class AuthController {
   async signIn(request: Request, response: Response) {
+    const schema = Yup.object().shape({
+      username: Yup.string().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Validation fails" });
+    }
+
     const { username, password } = request.body;
 
     const user = await UserModel.findOne({ username });
--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,10 +1,20 @@
 import { hash } from "bcrypt";
 import { Request, Response } from "express";
+import * as Yup from "yup";
 
 import { UserModel } from "../models/User";
 
 class UserController {
   async create(request: Request, response: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      username: Yup.string().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Validation fails" });
+    }
+
     const { password } = request.body;
 
     const userExists = await UserModel.findOne({
